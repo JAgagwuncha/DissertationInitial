10bcc45cf7b9061ea29f8ff34bdde9728baf7644
Joseph Agagwuncha <jocom1@hotmail.co.uk>
2021-10-22 23:19:49 +0100

    initial

---
+++ dissertationinitial/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import ApexCharts from 'apexcharts'
+import Home from "./pages/Home";
+import Chart from "react-apexcharts"
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+   
+    </div>
+  );
+}
+
+export default App;
